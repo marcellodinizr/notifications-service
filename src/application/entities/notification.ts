aeb0b1e28e57bd2be8290942de78ec5c32ec191e
marcellodinizr <marcellodinizrocha@gmail.com>
2022-12-15 09:40:16 -0300

    feat: adding _id in Notification

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'node:crypto';
 import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
@@ -10,14 +11,20 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
-  // helpers -> if informed, we use it, otherwise we use the current date
+  // helpers: Replace -> if informed, we use it, otherwise we use the current date
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
+  }
+
+  public get id() {
+    return this._id;
   }
 
   public get recipientId(): string {
